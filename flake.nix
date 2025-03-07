4ceff433ccbc20ce8c61747e09c78b6516d68b49
Bradman1985 <108242735+Bradman1985@users.noreply.github.com>
2025-03-06 23:03:25 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nix-minecraft, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixos-hardware, nix-minecraft, flake-utils, ... }:
     let
       system = "x86_64-linux";
     in {
       nixosConfigurations.macbook-server = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
+          nixos-hardware.nixosModules.apple-macbook-pro-5-5
           nix-minecraft.nixosModules.minecraft-servers
           ./configuration.nix
           ./mcservers.nix
