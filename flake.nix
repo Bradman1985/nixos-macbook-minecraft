47199690c1d69c0ef11296aa5c998c91596254da
Bradman1985 <108242735+Bradman1985@users.noreply.github.com>
2025-03-07 23:49:06 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,17 @@
 {
-  description = "NixOS configuration for 2009 MacBook Pro Minecraft server";
-
+  description = "NixOS config for 2009 MacBook Pro server";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+    nix-minecraft.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, nixos-hardware, nix-minecraft, flake-utils, ... }:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, nix-minecraft, flake-utils, ... }:
+    let system = "x86_64-linux";
     in {
       nixosConfigurations.macbook-server = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [
-          nixos-hardware.nixosModules.apple-macbook-pro-5-5
-          nix-minecraft.nixosModules.minecraft-servers
-          ./configuration.nix
-          ./mcservers.nix
-        ];
-        specialArgs = { inherit nix-minecraft; };
+        modules = [ ./configuration.nix nix-minecraft.nixosModules.minecraft ];
       };
     };
 }
-​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
